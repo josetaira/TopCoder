730dbb8536d1e34f0e0e8caa7c5670da6f531fa5
Pem Taira <jose_taira@azeus.com>
2016-04-18 07:31:04 +0100

    Restructuring

--- PrintScheduler.java
+++ PrintScheduler.java
@@ -1,5 +1,28 @@
 /**
- * Created by azuser on 4/18/2016.
+ * Source: Topcoder - SRM 238
+ * Category: Easy
+ * Points: 200
+ * Passes System Test: true
+ * @author Jose Taira
  */
 public class PrintScheduler {
+
+    public String getOutput(String[] threads, String[] slices) {
+        StringBuilder sb = new StringBuilder();
+
+        int[] curr = new int[threads.length];
+        for(int i = 0; i < slices.length; i++) {
+            String[] args = slices[i].split(" ");
+            int limit = Integer.valueOf(args[1]);
+            int threadIdx = Integer.valueOf(args[0]);
+            while(limit > 0) {
+                curr[threadIdx] = curr[threadIdx] % threads[threadIdx].length();
+                sb.append(threads[threadIdx].charAt(curr[threadIdx]++));
+                limit--;
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
